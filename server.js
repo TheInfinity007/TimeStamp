e7cf09bbe46dff6092bc6483154798fdbfa8370e
TheInfinity007 <ramchandra71999@gmail.com>
2020-07-31 15:09:00 +0530

    final commit

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -24,9 +18,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/:date_string', (req, res)=>{
+	let date_string = req.params.date_string;
+	let date;
+	if(date_string.indexOf("-") === -1)
+		date = new Date(parseInt(date_string));
+	else
+		date = new Date(date_string);	
+	if(date == "Invalid Date"){
+		 console.log("This is a invalid date");
+		 return res.json({"error": "Invalid Date"})	
+	}
+	res.json({
+		"unix": date.getTime(),
+		"utc": date.toGMTString()
+	});
+})
+
+app.get('/api/timestamp/*', (req, res)=>{
+	console.log("Timestamp route called");
+	let result = {
+		"unix": new Date().getTime(),
+		"utc": new Date().toGMTString()
+	}
+	res.send(result);
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
